e87b7c6ce2d563ecd339295e492cf386e906a0dd
Nathan Clark <nc8774@gmail.com>
2024-09-14 15:07:06 -0400

    dummy test for routing with gh-pages

--- vite-src/src/App.tsx
+++ vite-src/src/App.tsx
@@ -1,12 +1,31 @@
 import './App.css'
-import Cards from './Cards'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AboutMe from './pages/AboutMe';
+// import WorkExperience from './pages/WorkExperience';
+// import Projects from './pages/Projects';
+// import Contact from './pages/Contact';
+import Assignments from './pages/Assignments';
 
 function App() {
   return (
-    <>
-      <Cards />
-    </>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        {/* <Route path="/work-experience" element={<WorkExperience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/assignments" element={<Assignments />} />
+      </Routes>
+    </Router>
+  );
+  // return (
+  //   <>
+  //     <Cards />
+  //   </>
+  // )
 }
 
 export default App
