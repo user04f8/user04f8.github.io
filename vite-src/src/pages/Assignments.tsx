a0eaae5cce1123432fa1410289696dfe7b03a288
Nathan Clark <nc9241@gmail.com>
2024-09-24 07:45:10 -0700

    adding assignment descriptors

--- vite-src/src/pages/Assignments.tsx
+++ vite-src/src/pages/Assignments.tsx
@@ -1,6 +1,10 @@
 function Assignments() {
     const latestAssignment = 1;
     const assignments = Array.from({ length: latestAssignment + 1 }, (_, i) => i);
+    const assignment_descriptors = [
+      "This is a simple assignment used as a test.",
+      "This is the assignment on data colection and analysis."
+    ]
   
     return (
       <div className="mt-20 p-6 text-white">
@@ -9,7 +13,7 @@ function Assignments() {
           {assignments.map((num) => (
             <div key={num}>
               <h2 className="text-2xl font-semibold">Assignment {num}</h2>
-              <p className="mt-2"></p>
+              <p className="mt-2">assignment_descriptors[num]</p>
               <a
                 href={`https://github.com/user04f8-cs506/nbclark-assignment-${num}`}
                 className="text-blue-500 underline"
