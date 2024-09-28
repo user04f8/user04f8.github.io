12128f48ca98005d78d1ab844bbc377e398097ec
Nathan Clark <nc8774@gmail.com>
2024-09-27 21:10:31 -0400

    adding obsfucate

--- vite-src/vite.config.ts
+++ vite-src/vite.config.ts
@@ -1,18 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // @ts-ignore
-import obfuscator from 'vite-plugin-obfuscator';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      globalOptions: {
-        compact: true, // Minifies the output
-        controlFlowFlattening: true, // Adds control flow flattening to make the code harder to read
-      },
-      include: ['**/*.js', '**/*.jsx']
+    obfuscatorPlugin({
+      options: {
+        debugProtection: true, // Minifies the output
+      }
     })
   ],
 })
